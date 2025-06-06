61910b444f41998c7bb24fe2ddf5366a7c461990
Lovro Suhadolnik <lovro.suhadolnik@gmail.com>
2025-06-06 23:41:18 +0200

    build: include all pages in output

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,21 @@
 import { defineConfig } from 'vite';
 import { crx } from '@crxjs/vite-plugin';
 import manifest from './manifest.json';
+import { resolve } from 'path';
 
 export default defineConfig({
   build: {
-    outDir: 'dist'
+    outDir: 'dist',
+    rollupOptions: {
+      input: {
+        options: resolve(__dirname, 'app/pages/options.html'),
+        grid: resolve(__dirname, 'app/pages/grid.html'),
+        organisationdetails: resolve(__dirname, 'app/pages/organisationdetails.html'),
+        processes: resolve(__dirname, 'app/pages/processes.html'),
+        userroles: resolve(__dirname, 'app/pages/userroles.html'),
+        optionsets: resolve(__dirname, 'app/pages/optionsets.html'),
+      },
+    },
   },
-  plugins: [crx({ manifest })]
+  plugins: [crx({ manifest })],
 });
-
