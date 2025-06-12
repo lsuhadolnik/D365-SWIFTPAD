2ebba53490479a7fa7cc262421e35446d34774bc
Lovro Suhadolnik <lovro.suhadolnik@gmail.com>
2025-06-12 10:14:07 +0200

    Fix List.js usage

--- vite.config.ts
+++ vite.config.ts
@@ -22,7 +22,7 @@ export default defineConfig({
         processesScript: resolve(__dirname, 'app/scripts/processes.js'),
         userrolesScript: resolve(__dirname, 'app/scripts/userroles.js'),
         optionsetsScript: resolve(__dirname, 'app/scripts/optionsets.js'),
-        listLib: resolve(__dirname, 'app/libraries/list.es.js'),
+        listLib: resolve(__dirname, 'app/libraries/list.min.js'),
       },
       output: {
         entryFileNames: (chunk) => {
@@ -32,7 +32,7 @@ export default defineConfig({
             processesScript: 'app/scripts/processes.js',
             userrolesScript: 'app/scripts/userroles.js',
             optionsetsScript: 'app/scripts/optionsets.js',
-            listLib: 'app/libraries/list.es.js',
+            listLib: 'app/libraries/list.min.js',
           };
           return mapping[chunk.name] || 'assets/[name]-[hash].js';
         },
