d9212235ca526567ba48aad5ba6b71213c6f33e2
Lovro Suhadolnik <lovro.suhadolnik@gmail.com>
2025-06-12 13:21:59 +0200

    Improve Playwright harness and config

--- playwright.config.ts
+++ playwright.config.ts
@@ -1,8 +1,22 @@
 import { PlaywrightTestConfig } from '@playwright/test';
+import path from 'path';
+import { fileURLToPath } from 'url';
+
+const __dirname = path.dirname(fileURLToPath(import.meta.url));
+const extensionPath = path.join(__dirname, 'dist');
 
 const config: PlaywrightTestConfig = {
   testDir: './tests',
-  use: { headless: true },
+  use: {
+    headless: false,
+    launchOptions: {
+      args: [
+        `--disable-extensions-except=${extensionPath}`,
+        `--load-extension=${extensionPath}`,
+        '--allow-file-access-from-files',
+      ],
+    },
+  },
 };
 
 export default config;
